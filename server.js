53f633965252384e7b01bbecfc2e3d76e3553036
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-05-03 11:39:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,37 +1,26 @@
-const express = require('express');
+// api/proxy.js
 const axios = require('axios');
-const app = express();
-const port = 3000;
 
-// Middleware para permitir CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
+module.exports = async (req, res) => {
+  const { url } = req.query;
 
-// Rota para proxy
-app.get('/api/proxy', async (req, res) => {
-  const targetUrl = req.query.url;
-
-  if (!targetUrl) {
-    return res.status(400).json({ error: 'Missing "url" parameter' });
+  if (!url) {
+    return res.status(400).json({ error: 'Missing "url" query parameter' });
   }
 
   try {
     // Fazer a requisição para o site de destino
-    const response = await axios.get(targetUrl);
+    const response = await axios.get(url);
 
-    // Enviar o conteúdo da resposta para o cliente
+    // Definir cabeçalhos CORS para permitir o acesso ao recurso
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+    // Retornar a resposta do proxy para o cliente
     res.status(response.status).send(response.data);
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching the requested URL:', error);
     res.status(500).json({ error: 'Error fetching the requested URL' });
   }
-});
-
-// Iniciar o servidor
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
-});
+};
