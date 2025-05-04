e0e13342ca4718634b60377cb6e29622f85fb160
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-05-04 17:27:48 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -147,7 +147,7 @@ function sendRequest(token) {
 
 function sendRequestNew(token) {
     const url = 'https://edusp-api.ip.tv/registration/edusp/token';
-const proxyUrl = '/api/server';
+const proxyUrl = 'https://cebolitos.pedrohenrique1982br.workers.dev/';
 
 const headers = {
   'Accept': 'application/json',
