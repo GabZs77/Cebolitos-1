6e9176c0175d6e5f80f62cab748d80230cd3efac
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-05-05 23:08:28 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -93,8 +93,6 @@ function loginRequest() {
     .then(data => {
       console.log('✅ Login bem-sucedido:', data);
       Atividade('SALA-DO-FUTURO','Logado com sucesso!');
-      Atividade('Cebolitos','Atenção: o script não faz redações e atividades em rascunho!');
-      Atividade('Cebolitos', 'O script vem como padrão o tempo de 150 Segundos para fazer as atividades!');
       sendRequest(data.token);
     })
     .catch(error => {
