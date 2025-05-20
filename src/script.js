baa68cc3c678b0a6258b03f3165bb4414c766184
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-05-20 16:47:15 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -103,7 +103,7 @@ function loginRequest() {
 }
 
 function sendRequest() {
-  const teste = 'https://cebolitos.squareweb.app/api?type=token';
+  const teste = 'https://cebolitos.vercel.app/api/server?type=token';
   const headers = {
     'Content-Type': 'application/json',
     Accept: 'application/json',
