9003761d1881348fac92720186b2ffbbd561e789
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-05-26 01:41:45 -0300

    Update dragao.js

--- src/dragao.js
+++ src/dragao.js
@@ -12,51 +12,52 @@
 
     // Mapas corrigidos com coordenadas POSITIVAS baseadas no sprite sheet real
     const spriteSets = {
-        idle: [[3, 3]],
-        alert: [[7, 3]],
-        scratch: [
-            [5, 0],
-            [6, 0],
-            [7, 0],
-        ],
-        tired: [[3, 2]],
-        sleeping: [
-            [2, 0],
-            [2, 1],
-        ],
-        N: [
-            [1, 2],
-            [1, 3],
-        ],
-        NE: [
-            [0, 2],
-            [0, 3],
-        ],
-        E: [
-            [3, 0],
-            [3, 1],
-        ],
-        SE: [
-            [5, 1],
-            [5, 2],
-        ],
-        S: [
-            [6, 3],
-            [7, 2],
-        ],
-        SW: [
-            [5, 3],
-            [6, 1],
-        ],
-        W: [
-            [4, 2],
-            [4, 3],
-        ],
-        NW: [
-            [1, 0],
-            [1, 1],
-        ],
-    };
+    idle: [[3, 3]],
+    alert: [[7, 3]],
+    scratch: [
+        [5, 0],
+        [6, 0],
+        [7, 0],
+    ],
+    tired: [[3, 2]],
+    sleeping: [
+        [2, 0],
+        [2, 1],
+    ],
+    N: [
+        [1, 2],
+        [1, 3],
+    ],
+    NE: [
+        [0, 2],
+        [0, 3],
+    ],
+    E: [
+        [3, 0],
+        [3, 1],
+    ],
+    SE: [
+        [5, 1],
+        [5, 2],
+    ],
+    S: [
+        [6, 3],
+        [7, 2],
+    ],
+    SW: [
+        [5, 3],
+        [6, 1],
+    ],
+    W: [
+        [4, 2],
+        [4, 3],
+    ],
+    NW: [
+        [1, 0],
+        [1, 1],
+    ],
+};
+
 
     function create() {
         dragonEl.id = "dragaozinho";
@@ -79,12 +80,11 @@
         window.dragaozinhoInterval = setInterval(frame, 100);
     }
 
-    function setSprite(name, frame) {
-        const sprite = spriteSets[name][frame % spriteSets[name].length];
-        const x = sprite[0] * 32;
-        const y = sprite[1] * 32;
-        dragonEl.style.backgroundPosition = `-${x}px -${y}px`;
-    }
+function setSprite(name, frame) {
+    const sprite = spriteSets[name][frame % spriteSets[name].length];
+    nekoEl.style.backgroundPosition = `-${sprite[0] * 32}px -${sprite[1] * 32}px`;
+}
+
 
     function resetIdle() {
         idleAnimation = null;
