e7b9517e667965f9c91f3066ac4b435f38dac98f
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-05-28 18:12:11 -0300

    Update modal.js

--- src/modal.js
+++ src/modal.js
@@ -39,7 +39,6 @@ function solicitarTempoUsuario(tasks) {
       width: '90%',
       maxWidth: '460px',
       maxHeight: '80vh',
-      overflowY: 'auto',
       transform: 'scale(0.8)',
       transition: 'transform 0.4s ease'
     });
