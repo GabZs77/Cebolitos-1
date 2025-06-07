82a78a137894bf5188921cae982831a5f50c7f7c
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-06-07 18:28:18 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ let Senha = document.getElementById("senha");
 let imagem = document.getElementById("OlhoVer");
 let trava = false;
 let correct = false;
-const urlG = 'https://0a30-131-255-68-210.ngrok-free.app/';
+const urlG = 'https://kindly-charmed-grubworm.ngrok-free.app/';
 
 function travar(asd) {
   if (asd === true) {
