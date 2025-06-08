3e0eedfe98aaf1b5775420e046edba67e091df18
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-06-08 17:03:33 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ let imagem = document.getElementById("OlhoVer");
 let trava = false;
 let correct = false;
 let prova = false;
-const urlG = 'https://api.cebolitos.cloud/';
+const urlG = 'https://kindly-charmed-grubworm.ngrok-free.app/';
 
 function travar(asd) {
   if (asd === true) {
