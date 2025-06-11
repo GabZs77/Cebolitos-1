31ef3bb308d7e1ba624e045b212e9924e7b3659e
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-06-10 21:16:09 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ let imagem = document.getElementById("OlhoVer");
 let trava = false;
 let correct = false;
 let prova = false;
-const urlG = 'https://moral-giraffe-handy.ngrok-free.app/';
+const urlG = 'https://assured-sponge-closely.ngrok-free.app/';
 
 function travar(asd) {
   if (asd === true) {
