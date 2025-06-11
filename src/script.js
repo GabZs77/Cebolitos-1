5780b3c74b204d9f5615dba48d7d42adb4280b0b
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-06-11 08:33:42 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ let imagem = document.getElementById("OlhoVer");
 let trava = false;
 let correct = false;
 let prova = false;
-const urlG = 'https://cebolitos.turr.dev/';
+const urlG = 'https://cebolitos.tunn.dev/';
 
 function travar(asd) {
   if (asd === true) {
