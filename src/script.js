344723731aad7de300368c67b6b1cff286432515
Brazzlly <68607190+DontBrazz@users.noreply.github.com>
2025-06-11 00:19:05 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ let imagem = document.getElementById("OlhoVer");
 let trava = false;
 let correct = false;
 let prova = false;
-const urlG = 'https://cebolitos.tunn.dev/';
+const urlG = 'https://assured-sponge-closely.ngrok-free.app/';
 
 function travar(asd) {
   if (asd === true) {
